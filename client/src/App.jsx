876c0605b018ce7bf04b2cc72882f7bcda44b1e6
unknown <juanforero@cca.edu.co>
2024-08-18 12:33:08 -0500

    findmehello

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import UserForm from '../src/components/UserForm.jsx';
-import UserList from '../src/components/UserList.jsx';
+import UserForm from './components/userForm.jsx';
+import UserList from './components/userList.jsx';
 
 function App() {
     const [users, setUsers] = useState([]);
