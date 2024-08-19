bc1dd464066c84490c6b10923faaf9042e2f1c3a
unknown <juanforero@cca.edu.co>
2024-08-18 22:37:28 -0500

    euofhsfu

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,9 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': 'https://mern-username-app.onrender.com'
-    }
-  }
+  
 })
